fcafad9fd3e04cbc77af5a2a65d5212a552f6ae8
Kevin Thompson <indelible11@yahoo.com>
2019-11-22 01:55:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  top_rappers.unshift "Biggie"
+  array.unshift "Biggie"
 end
 
 def remove_element_from_end_of_array(array)
