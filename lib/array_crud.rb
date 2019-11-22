169c2a1146b740ba86252a15192df14874aec4d5
Kevin Thompson <indelible11@yahoo.com>
2019-11-22 01:39:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,27 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
-  
+  ["Jay-z", "Lil Wayne", "J. Cole", "Rick Ross"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["Jay-z", "Lil Wayne", "J. Cole", "Rick Ross"]
+    
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  top_rappers.unshift "Biggie"
 end
 
 def remove_element_from_end_of_array(array)
-  
+  top_rappers.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  
+  top_rappers.shift
 end
 
 def retrieve_element_from_index(array, index_number)
